bf02c1de53434c25568e15146763b13b2d94adf4
ham <1319716110@qq.com>
2024-07-02 09:40:24 +0800

    添加测试模板工程

---
+++ examination_system/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import HomeView from "@/views/HomeView.vue";
+
+
+</script>
+
+
+<template>
+  <div>
+<!--    <RouterView></RouterView>-->
+    <HomeView></HomeView>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
