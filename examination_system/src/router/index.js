96154dc5548ae476846638af6c4c1edf7646e6cc
plbz <37520222204740@stu.xmu.edu.cn>
2024-07-10 20:24:59 +0800

    试卷界面

--- examination_system/src/router/index.js
+++ examination_system/src/router/index.js
@@ -4,13 +4,13 @@ import HomeView from '../views/HomeView.vue'
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-        { path: '/', name: 'home', component: () => import('../views/UserView.vue')},
+        { path: '/', name: 'home', component: () => import('../views/IndexView.vue')},
         { path: '/team', name: 'team', component: () => import('../views/TeamListView.vue')},
         { path: '/exam', name: 'exam', component: () => import('../views/ExamListView.vue')},
         { path: '/about', name: 'about', component: () => import('../views/AboutView.vue')},
         { path: '/question', name: 'question', component: () => import('../views/QuestionListView.vue')},
         { path: '/paper', name: 'paper', component: () => import('../views/PaperView.vue')},
-        { path: '/history', name: 'history', component: () => import('../views/HistoryView.vue')},
+        { path: '/admin/users', name: 'admin/users', component: () => import('../views/UserView.vue')},
         { path: '/:pathMatch(.*)*', name: 'noFound', component: () => import('../views/NoFoundVue.vue')}
     ]
 })
